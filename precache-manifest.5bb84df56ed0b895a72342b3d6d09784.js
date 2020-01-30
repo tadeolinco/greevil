f05395f9a3f4c51e2bcbdfb79a7fc1154c95a84a
Samuel Bautista <sbautista@stratpoint.com>
2020-01-31 02:03:53 +0800

    Updates

---
+++ precache-manifest.5bb84df56ed0b895a72342b3d6d09784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9688c44c4dbe7888860b5e7bd908a8e",
+    "url": "/greevil/index.html"
+  },
+  {
+    "revision": "440b49ce25e46dad767d",
+    "url": "/greevil/static/css/2.18d8decb.chunk.css"
+  },
+  {
+    "revision": "440b49ce25e46dad767d",
+    "url": "/greevil/static/js/2.488d6b40.chunk.js"
+  },
+  {
+    "revision": "c46a340a0b729bea2c2fc17f9f8c09a2",
+    "url": "/greevil/static/js/2.488d6b40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "62769a53550e35c0eba4",
+    "url": "/greevil/static/js/main.0ca66b96.chunk.js"
+  },
+  {
+    "revision": "209f219fdd7ad8dbadd9",
+    "url": "/greevil/static/js/runtime-main.b29d6a6e.js"
+  }
+]);
